5b5d1d8b4881c3034260221053d5c3e71ce88b64
jhwnkim <jhwnkim@mit.edu>
2019-09-04 20:09:16 -0400

    Create __init__.py

---
+++ instrumental/drivers/microcontroller/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Jaehwan Kim
+"""
+Package containing a driver module/class for supported microcontrollers.
+"""
+from .. import Instrument
+
+
+class Microcontroller(Instrument):
+    pass
