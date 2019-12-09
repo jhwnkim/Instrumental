9c462e083678688b1683c3d0f8048ca9f8f3573c
Jaehwan Kim <jhwnkim@mit.edu>
2019-12-09 01:04:44 -0500

    Modified diconopticalswitch to initialize maximum output channels by communicating with the device

--- instrumental/drivers/opticalswitch/diconopticalswitch.py
+++ instrumental/drivers/opticalswitch/diconopticalswitch.py
@@ -11,6 +11,7 @@ switch.close()
 
 from . import OpticalSwitch
 from serial import Serial
+import re
 
 class DiConOpticalSwitch(OpticalSwitch):
     _INST_PARAMS_ = ['port']
@@ -18,10 +19,13 @@ class DiConOpticalSwitch(OpticalSwitch):
     def _initialize(self):
         self._ser = Serial(self._paramset['port'], baudrate=115200, timeout=1.0)
         self._channel = 0
-        self._channel_max = 16
-        self._channel_min = 1
-        # *CLS, *RST resets current settings so don't
-        # self.write('*CLS')
+
+        # Read number of channels from module
+        self._ser.reset_input_buffer()
+        self._ser.write(b'CF?\r')
+        self._ser.read() # dummy read the newline
+        self._channel_max = int(re.split(',', self._ser.readline())[1])
+        # self._channel_max = 16
 
     def identify(self):
         self._ser.reset_input_buffer()
@@ -30,7 +34,7 @@ class DiConOpticalSwitch(OpticalSwitch):
         print(self._ser.readline())
 
     def set_channel(self, new_channel):
-        if new_channel >=self._channel_min and new_channel <= self._channel_max:
+        if new_channel >=0 and new_channel <= self._channel_max:
             self._channel = new_channel
             self._ser.reset_input_buffer()
             self._ser.write(bytes('I1 {}\r'.format(new_channel), 'utf-8'))
