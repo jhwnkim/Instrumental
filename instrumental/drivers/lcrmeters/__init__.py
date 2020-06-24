a8661c454383b7e0c15e8236ea3cf9e5d5f45477
jhwnkim <jhwnkim@mit.edu>
2020-06-24 19:33:15 -0400

    created lcr-meter

---
+++ instrumental/drivers/lcrmeters/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Jaehwan Kim
+"""
+Package containing a driver module/class for supported LCR meters.
+"""
+from .. import Instrument
+
+class LCRMeter(Instrument):
+    pass
