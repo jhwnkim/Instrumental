78aa9609a14bef7bce41bd628aa4c966674d3fc5
jhwnkim <jhwnkim@mit.edu>
2020-07-17 20:42:24 -0400

    Update hp.py to support 2 channels

--- instrumental/drivers/powermeters/hp.py
+++ instrumental/drivers/powermeters/hp.py
@@ -31,10 +31,12 @@ class HP_8153A(PowerMeter, VisaMixin):
         self.int_time = 0.05 # seconds
 
         # Set units to W
+        self.write("SENS1:POW:UNIT 1")
         self.write("SENS2:POW:UNIT 1")
         # self.write("SENS%d:CHAN1:POW:UNIT 1" % self.rec_channel)
 
         # Automatic power range
+        self.write("SENS1:POW:RANG:AUTO 1" )
         self.write("SENS2:POW:RANG:AUTO 1" )
         # self.write("SENS%d:CHAN1:POW:RANG:AUTO 1" % self.rec_channel)
 
@@ -43,6 +45,7 @@ class HP_8153A(PowerMeter, VisaMixin):
         # self.write("SENS%d:CHAN1:POW:ATIME %.3fS" % (self.rec_channel, self.int_time))
 
         # Do not measure continuously
+        self.write("INIT1:CONT 0")
         self.write("INIT2:CONT 0")
         # self.write("INIT%d:CHAN1:CONT 0" % self.rec_channel)
 
@@ -60,20 +63,38 @@ class HP_8153A(PowerMeter, VisaMixin):
     # power = SCPI_Facet('POW', units='W', readonly=True, type=float,
     #                         doc="Measured Power")
     # Not using SCPI_Facet because the value gets stuck when auto ranging
-    def power(self):
-        self.write("INIT2:IMM")
-        try:
-            meas_power = max(0.0, float(self.query('FETC2:POW?')))
-        except ValueError:
-            meas_power = 0.0
+    def power(self, channel=2):
+        if channel in [1,2]:
+            self.write("INIT{}:IMM".format(channel))
+            try:
+                meas_power = max(0.0, float(self.query('FETC{}:POW?'.format(channel))))
+            except ValueError:
+                meas_power = 0.0
 
-        return Q_(meas_power, units='W')
+            return Q_(meas_power, units='W')
 
-    wavelength = SCPI_Facet('SENS2:POW:WAVE', units='nm', type=float,
+        else:
+            print("channel {} not available. select 1 or 2".format(channel))
+            return Q_(0, 'W')
+
+    def set_channel(self, channel=2):
+
+
+
+    wavelength2 = SCPI_Facet('SENS2:POW:WAVE', units='nm', type=float,
                             doc="Input signal wavelength")
 
-    auto_range = SCPI_Facet('SENS2:POW:RANG:AUTO', convert=int, value={False:0, True:1},
+    auto_range2 = SCPI_Facet('SENS2:POW:RANG:AUTO', convert=int, value={False:0, True:1},
                             doc="Whether auto-ranging is enabled")
 
-    integration_time = SCPI_Facet('SENS2:POW:ATIME', units='s', type=float,
+    integration_time2 = SCPI_Facet('SENS2:POW:ATIME', units='s', type=float,
+                            doc="Measurement integration time in seconds 20ms to 3600s available")
+
+    wavelength1 = SCPI_Facet('SENS1:POW:WAVE', units='nm', type=float,
+                            doc="Input signal wavelength")
+
+    auto_range1 = SCPI_Facet('SENS1:POW:RANG:AUTO', convert=int, value={False:0, True:1},
+                            doc="Whether auto-ranging is enabled")
+
+    integration_time1 = SCPI_Facet('SENS1:POW:ATIME', units='s', type=float,
                             doc="Measurement integration time in seconds 20ms to 3600s available")
